02e5798cc32151f678b717772c35311bdf05647b
Onergrand <saveliy.kuzmin@mail.ru>
2023-05-19 23:25:25 +0500

    added wait before gameState change

--- RoguelikeGame/GameView/GameCycleView.cs
+++ RoguelikeGame/GameView/GameCycleView.cs
@@ -23,15 +23,16 @@ public class GameCycleView : Game, IGameView
     public event EventHandler ChangedGameState;
 
     private Dictionary<int, IEntity> _entities = new();
-    private GameState _currentGameState = new();
+    private GameState _currentGameState;
     private readonly Dictionary<int, Texture2D> _textures = new();
     
     private readonly GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
-
+    private DateTime _lastTimeExitButtonPressed = DateTime.Now;
+    
     private Vector2 _visualShift = new(
-        Level.InitialPos.X * Level.TileSize - 50,
-        Level.InitialPos.Y * Level.TileSize - 50);
+        Level.InitialPos.X * Level.TileSize - Level.TileSize,
+        Level.InitialPos.Y * Level.TileSize - Level.TileSize);
 
     public GameCycleView()
     {
@@ -75,44 +76,81 @@ public class GameCycleView : Game, IGameView
 
         foreach (var key in pressedKeys)
         {
-            switch (key)
-            {
-                case Keys.W:
-                    PlayerMoved!(this, new ControlsEventArgs { Direction = Direction.North });
-                    break;
-                case Keys.S:
-                    PlayerMoved!(this, new ControlsEventArgs { Direction = Direction.South });
-                    break;
-                case Keys.D:
-                    PlayerMoved!(this, new ControlsEventArgs { Direction = Direction.East });
-                    break;
-                case Keys.A:
-                    PlayerMoved!(this, new ControlsEventArgs { Direction = Direction.West });
-                    break;
-                
-                case Keys.Up:
-                    PlayerAttacked!(this, new ControlsEventArgs { Direction = Direction.North });
-                    break;
-                case Keys.Right:
-                    PlayerAttacked!(this, new ControlsEventArgs { Direction = Direction.East });
-                    break;
-                case Keys.Left:
-                    PlayerAttacked!(this, new ControlsEventArgs { Direction = Direction.West });
-                    break;
-                case Keys.Down:
-                    PlayerAttacked!(this, new ControlsEventArgs { Direction = Direction.South });
-                    break;
-                
-                case Keys.Escape:
-                    ChangedGameState!(this, EventArgs.Empty);
-                    break;
-            }
+            if(_currentGameState == GameState.Game)
+                CheckInGameButtons(key);
+            else
+                CheckInMenuButtons(key);
         }
         
         base.Update(gameTime);
         CycleFinished!(this, EventArgs.Empty);
     }
+
+    private void CheckInGameButtons(Keys key)
+    {
+        switch (key)
+        {
+            case Keys.W:
+                PlayerMoved!(this, new ControlsEventArgs { Direction = Direction.North });
+                break;
+            case Keys.S:
+                PlayerMoved!(this, new ControlsEventArgs { Direction = Direction.South });
+                break;
+            case Keys.D:
+                PlayerMoved!(this, new ControlsEventArgs { Direction = Direction.East });
+                break;
+            case Keys.A:
+                PlayerMoved!(this, new ControlsEventArgs { Direction = Direction.West });
+                break;
+                
+            case Keys.Up:
+                PlayerAttacked!(this, new ControlsEventArgs { Direction = Direction.North });
+                break;
+            case Keys.Right:
+                PlayerAttacked!(this, new ControlsEventArgs { Direction = Direction.East });
+                break;
+            case Keys.Left:
+                PlayerAttacked!(this, new ControlsEventArgs { Direction = Direction.West });
+                break;
+            case Keys.Down:
+                PlayerAttacked!(this, new ControlsEventArgs { Direction = Direction.South });
+                break;
+                
+            case Keys.Escape:
+                if ((DateTime.Now - _lastTimeExitButtonPressed).Milliseconds < 150) 
+                    break;
+                
+                ChangedGameState!(this, EventArgs.Empty);
+                _lastTimeExitButtonPressed = DateTime.Now;
+                break;
+        }
+    }
+    
+    private void CheckInMenuButtons(Keys key)
+    {
+        if ((DateTime.Now - _lastTimeExitButtonPressed).Milliseconds < 150) return;
+        
+        if (key == Keys.Escape) 
+            ChangedGameState!(this, EventArgs.Empty);
+        
+        _lastTimeExitButtonPressed = DateTime.Now;
+    }
+    
     protected override void Draw(GameTime gameTime)
+    {
+         if (_currentGameState == GameState.Menu)
+             DrawMenu(gameTime);
+         else
+             DrawGame(gameTime);
+    }
+
+    private void DrawMenu(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(new Color(177, 180, 186));
+        base.Draw(gameTime);
+    }
+
+    private void DrawGame(GameTime gameTime)
     {
         GraphicsDevice.Clear(new Color(177, 180, 186));
         base.Draw(gameTime);
@@ -129,6 +167,6 @@ public class GameCycleView : Game, IGameView
         foreach (var o in _entities.Values.Except(floor).Except(walls))
             _spriteBatch.Draw(_textures[o.ImageId], o.Position - _visualShift, Color.White);
 
-        _spriteBatch.End();            
+        _spriteBatch.End();   
     }
 }
